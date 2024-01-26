3991a1e113de95b25768cfecf59519e92a9cbc50
김지혜 <toto7971@reflexion.co.kr>
2024-01-26 17:58:57 +0900

    todo list 기본 구현 및 연결

--- vue3-todo/src/components/TodoList.vue
+++ vue3-todo/src/components/TodoList.vue
@@ -1,1 +1,20 @@
+<template>
+  <ul>
+    <li v-for="(item, index) in items" :key="index">{{ item }}</li>
+  </ul>
+</template>
 
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    //data
+    const items = ref(["aa", "bb", "cc"]);
+
+    return { items };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
